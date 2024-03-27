caa8f5613553093e9134c8a474de04301220aaa5
MatiasO <debian4tw@gmail.com>
2024-03-27 01:11:35 -0300

    core: test run ci

--- packages/core/src/tests/SpawnLocationManager.test.ts
+++ packages/core/src/tests/SpawnLocationManager.test.ts
@@ -1,74 +1,84 @@
-import { SpawnLocationManager} from '../scenario/SpawnLocation.manager'
-import { ISpawnLocationDef } from '../scenario/ISpawnLocationManager'
-import { Axis } from '@cuboid3/g-physics'
+import { SpawnLocationManager } from "../scenario/SpawnLocation.manager";
+import { ISpawnLocationDef } from "../scenario/ISpawnLocationManager";
+import { Axis } from "@cuboid3/g-physics";
 
-import {spawnLocations} from '../scenario/SpawnLocations'
+import { spawnLocations } from "../scenario/SpawnLocations";
 
-describe('SpawnLocationManager test', function() {
+describe("SpawnLocationManager test", function () {
+  const manager = new SpawnLocationManager(spawnLocations);
+  const slotsAmount = spawnLocations.length;
 
-  const manager = new SpawnLocationManager(spawnLocations)
-  const slotsAmount = spawnLocations.length
+  it("all slots should be free", function () {
+    const freeSlots = manager.getSlots().filter((slot) => slot.busy === false);
+    expect(freeSlots.length).toBe(slotsAmount);
+  });
 
+  it("one slot should be busy", function () {
+    manager.getNextAvailable();
+    const freeSlots = manager.getSlots().filter((slot) => slot.busy === false);
+    expect(freeSlots.length).toBe(slotsAmount - 1);
+  });
 
-  it('all slots should be free', function() {
-    const freeSlots = manager.getSlots().filter(slot => slot.busy === false)
-    expect(freeSlots.length).toBe(slotsAmount)
-  })
+  it("two slots should be busy", function () {
+    manager.getNextAvailable();
+    const freeSlots = manager.getSlots().filter((slot) => slot.busy === false);
+    expect(freeSlots.length).toBe(slotsAmount - 2);
+  });
+});
 
-  it('one slot should be busy', function() {
-    manager.getNextAvailable()
-    const freeSlots = manager.getSlots().filter(slot => slot.busy === false)
-    expect(freeSlots.length).toBe(slotsAmount-1)
-  })
+describe("SpawnLocationManager test with all busy slots", function () {
+  const manager = new SpawnLocationManager(spawnLocations);
+  const slotsAmount = spawnLocations.length;
+  manager.getNextAvailable();
+  manager.getNextAvailable();
+  manager.getNextAvailable();
+  manager.getNextAvailable();
+  manager.getNextAvailable();
+  manager.getNextAvailable();
+  manager.getNextAvailable();
 
-  it('two slots should be busy', function() {
-    manager.getNextAvailable()
-    const freeSlots = manager.getSlots().filter(slot => slot.busy === false)
-    expect(freeSlots.length).toBe(slotsAmount-2)
-  })
-})
+  it("all slots should be busy", function () {
+    const freeSlots = manager.getSlots().filter((slot) => slot.busy === false);
+    expect(freeSlots.length).toBe(0);
+  });
 
+  it("should return a slot, even all are busy", function () {
+    const location = manager.getNextAvailable();
+    expect(location).toHaveProperty("loc");
+    expect(location).toHaveProperty("rot");
+  });
 
-describe('SpawnLocationManager test with all busy slots', function() {
+  it(
+    "slots should be busy after 3s",
+    async () => {
+      await new Promise((res) =>
+        setTimeout(() => {
+          const freeSlots = manager
+            .getSlots()
+            .filter((slot) => slot.busy === false);
+          console.log("freeSlots 3s", freeSlots);
+          expect(freeSlots.length).toBe(0);
+          res();
+        }, 3 * 1000)
+      );
+    },
+    3 * 1000
+  );
 
-  const manager = new SpawnLocationManager(spawnLocations)
-  const slotsAmount = spawnLocations.length
-  manager.getNextAvailable()
-  manager.getNextAvailable()
-  manager.getNextAvailable()
-  manager.getNextAvailable()
-  manager.getNextAvailable()
-  manager.getNextAvailable()
-  manager.getNextAvailable()
-
-  it('all slots should be busy', function() {
-    const freeSlots = manager.getSlots().filter(slot => slot.busy === false)
-    expect(freeSlots.length).toBe(0)
-  })
-
-  it('should return a slot, even all are busy', function() {
-    const location = manager.getNextAvailable()
-    expect(location).toHaveProperty("loc")
-    expect(location).toHaveProperty("rot")
-  })
-
-
-  it('slots should be busy after 3s', async () => {
-    await new Promise(res => setTimeout(() => {
-      const freeSlots = manager.getSlots().filter(slot => slot.busy === false)
-      console.log("freeSlots 3s", freeSlots)
-      expect(freeSlots.length).toBe(0)
-      res()
-    }, 3 * 1000))
-  },3 * 1000)
-
-
-  it('slots should stop being busy after 8s', async () => {
-    await new Promise(res => setTimeout(() => {
-      const freeSlots = manager.getSlots().filter(slot => slot.busy === false)
-      console.log("freeSlots 8s", freeSlots)
-      expect(freeSlots.length).toBe(slotsAmount)
-      res()
-    }, 10 * 1000))
-  },10 * 1000)
-})
+  it(
+    "slots should stop being busy after 8s",
+    async () => {
+      await new Promise((res) =>
+        setTimeout(() => {
+          const freeSlots = manager
+            .getSlots()
+            .filter((slot) => slot.busy === false);
+          console.log("freeSlots 8s", freeSlots);
+          expect(freeSlots.length).toBe(slotsAmount);
+          res();
+        }, 10 * 1000)
+      );
+    },
+    10 * 1000
+  );
+});
